8b65e698ad7240b0092ff5173c7a4b9bceda83f1
Edward <64332463+AgreSSoor@users.noreply.github.com>
2021-02-22 20:44:12 +0300

    Created class Linkedlist with implement of interface IEnumerable<T>

---
+++ MyLinkedList/MyLinkedList/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace MyLinkedList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> first; // first element of list
+        private Node<T> last; // last element of list
+        private int count; // quantity of elements
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+ 
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            var current = first;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
--- MyLinkedList/MyLinkedList/Node.cs
+++ MyLinkedList/MyLinkedList/Node.cs
@@ -1,4 +1,4 @@
-﻿namespace LinkedList
+﻿namespace MyLinkedList
 {
     public class Node<T>
     {
